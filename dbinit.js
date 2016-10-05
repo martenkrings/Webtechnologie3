f48696b0330a69e90a80dbe71f294f63396e679c
Marten <krings.marten@gmail.com>
2016-10-05 14:40:10 +0200

    dbinit

---
+++ dbinit.js
@@ -0,0 +1,55 @@
+/**
+ * Created by Marten on 10/5/2016.
+ */
+var Film = require('./model/film.js');
+
+Film.remove({}, function(err) {
+    console.log('collection removed')
+});
+
+var f1 = Film({
+    ttNumber: "tt03877808",
+    title: "Idiocracy",
+    date: "2006/01/25",
+    length: 84,
+    director: "Mike Judge",
+    description: "Private Joe Bauers, the definition of \"average American\", is selected by the Pentagon to be the guinea pig for a top-secret hibernation program. Forgotten, he awakes five centuries in the future. He discovers a society so incredibly dumbed down that he's easily the most intelligent person alive."
+});
+
+f1.save(function(err) {
+    if (err) throw err;
+
+    console.log('film1 created');
+});
+
+var f2 = Film({
+    ttNumber: "tt0371724",
+    title: "The Hitchhiker's Guide to the Galaxy",
+    date: "2005/08/04",
+    length: 109,
+    director: "Garth Jennings",
+    description: "Mere seconds before the Earth is to be demolished by an alien construction crew, journeyman Arthur Dent is swept off the planet by his friend Ford Prefect, a researcher penning a new edition of \"The Hitchhiker's Guide to the Galaxy.\""
+});
+
+f2.save(function(err) {
+    if (err) throw err;
+
+    console.log('film2 created');
+});
+
+var f3 = Film({
+    ttNumber: "tt0119654",
+    title: "Men in Black",
+    date: "1997/07/24",
+    length: 98,
+    director: "Barry Sonnenfeld",
+    description: "A police officer joins a secret organization that polices and monitors extraterrestrial interactions on Earth."
+});
+
+f3.save(function(err) {
+    if (err) throw err;
+
+    console.log('film3 created');
+});
+
+module.exports = this;
--- index.js
+++ index.js
@@ -7,6 +7,8 @@ var bodyParser = require('body-parser');
 var jwt = require('jsonwebtoken');
 var mongoose = require('mongoose');
 
+var dbinit = require('./dbinit');
+
 mongoose.connect('mongodb://localhost/notflix');
 
 //import film router
@@ -23,49 +25,3 @@ app.listen(3000, function () {
 app.get('/', function (req, res) {
     res.send('Hello World!');
 });
-
-/**
- var db = {
-    movies: [
-        {
-            ttNumber: "tt03877808",
-            title: "Idiocracy",
-            date: "2006/01/25",
-            length: 84,
-            director: "Mike Judge",
-            desc: "Private Joe Bauers, the definition of \"average American\", is selected by the Pentagon to be the guinea pig for a top-secret hibernation program. Forgotten, he awakes five centuries in the future. He discovers a society so incredibly dumbed down that he's easily the most intelligent person alive."
-        },
-        {
-            ttNumber: "tt0371724",
-            title: "The Hitchhiker's Guide to the Galaxy",
-            date: "2005/08/04",
-            length: 109,
-            director: "Garth Jennings",
-            desc: "Mere seconds before the Earth is to be demolished by an alien construction crew, journeyman Arthur Dent is swept off the planet by his friend Ford Prefect, a researcher penning a new edition of \"The Hitchhiker's Guide to the Galaxy.\""
-        },
-        {
-            ttNumber: "tt0119654",
-            title: "Men in Black",
-            date: "1997/07/24",
-            length: 98,
-            director: "Barry Sonnenfeld",
-            desc: "A police officer joins a secret organization that polices and monitors extraterrestrial interactions on Earth."
-        }],
-    users: [
-        {
-            id: 1,
-            lastName: "Groot Wesseldijk",
-            insertion: "",
-            firstName: "Sander",
-            username: "heuker",
-            password: "password1"
-        },
-        {id: 2, lastName: "Krings", insertion: "", firstname: "Marten", username: "zixue", password: "password2"}
-    ],
-
-    ratings: [
-        {userId: 1, rating: 5, movieTtNumber: "tt0119654"},
-        {userId: 2, rating: 3, movieTtNumber: "tt0119654"}
-    ]
-};
- */
