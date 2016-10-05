5d1c2174ac54f492744dd592f3f5081dee25e926
Marten <krings.marten@gmail.com>
2016-10-05 11:32:31 +0200

    sdf

--- index.js
+++ index.js
@@ -2,8 +2,8 @@
  * Created by Marten on 10/5/2016.
  *
  */
-var app = express();
 var express = require('express');
+var app = express();
 var bodyParser = require('body-parser');
 var jwt = require('jsonwebtoken');
 var mongoose = require('mongoose');
