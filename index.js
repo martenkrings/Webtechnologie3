61b82d588ec5d0ae4c4395f0573265eaea682193
Marten <krings.marten@gmail.com>
2016-10-05 11:15:14 +0200

    added dependencies

--- index.js
+++ index.js
@@ -1,4 +1,8 @@
 /**
  * Created by Marten on 10/5/2016.
  */
-console.log("Hello World!!");
+var express = require('express');
+var app = express();
+var bodyParser = require('body-parser');
+var jwt = require('jsonwebtoken');
+var mongoose = require('mongoose');
