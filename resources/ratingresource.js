0ae4cc30156d4fccde73923943b862cd4ae8fa54
Sander Groot Wesseldijk <heukertje@gmail.com>
2016-10-11 20:10:29 +0200

    lelijke getRatings

--- resources/ratingresource.js
+++ resources/ratingresource.js
@@ -8,6 +8,45 @@ var User = require('../model/user');
 var Film = require('../model/film.js');
 var Rating = require('../model/rating');
 
+var result = [];
+var filmsProgressed = 0;
+
+var myCallBack = function (err, data, numberOfFilms, res) {
+    if (err) throw err;
+    result.push(data);
+    filmsProgressed++;
+    if (filmsProgressed == numberOfFilms) {
+        //send the result
+        console.log("RESULT = " + result);
+        res.contentType('application/json');
+        res.status(200).send(JSON.stringify(result));
+    }
+};
+
+var calculate = function (callback, i, filmProgressed, numberOfFilms, res) {
+    Rating.find({ttNumber: filmProgressed.ttNumber}, function (err, ratings) {
+        if (err) {
+            res.status(400).json({error: "Could not find ratings"})
+        } else {
+            //calculate the average
+            var average = 0;
+
+            for (var j = 0; j < ratings.length; j++) {
+                average = average + ratings[j].rating;
+            }
+
+            average = average / j;
+            console.log("average: " + average);
+
+            var filmWithAverage = {film: filmProgressed, averageRating: average};
+            console.log("filmWithAverage: " + filmWithAverage);
+            // result[i] = filmWithAverage;
+            console.log(result[i]);
+            callback(null, filmWithAverage, numberOfFilms, res);
+        }
+    });
+};
+
 /**
  * Get request that gets all movies with their average rating
  */
@@ -23,139 +62,142 @@ router.get("/", function (req, res) {
                 if (err) {
                     res.status(400).json({error: "Could not find films in databse"})
                 } else {
-                    var result = [];
+                    var numberOfFilms = filmResult.length;
 
-                    for (i = 0; i < filmResult.length; i++) {
-                        result.push(getGemiddeldeRating(filmResult[i], filmResult.ttNumber, i));
+                    console.log("axafaf");
+                    for (var i = 0; i < numberOfFilms; i++) {
+                        calculate(myCallBack, i, filmResult[i], numberOfFilms, res);
                     }
                 }
+            })
 
-                console.log("RESULT" + result);
-                res.contentType('application/json');
-                res.status(200).send(JSON.stringify(result));
+        }
+    });
+});
+
+// function getGemiddeldeRating(callback, film, foundTtNumber) {
+//     Rating.find({ttNumber: foundTtNumber}, function (err, ratings) {
+//         if (err) {
+//             res.status(400).json({error: "Could not find ratings"})
+//         } else {
+//             //calculate the average
+//             var average = 0;
+//             var numberOfRatings = 0;
+//
+//             for (var j = 0; j < ratings.length; j++) {
+//                 average = average + ratings[j].rating;
+//             }
+//
+//             average = average / j;
+//             console.log("average: " +
+//                 "" + average);
+//             console.log("film: " + film);
+//
+//             var filmWithAverage = {film: film, averageRating: average};
+//             // var filmWithAverage = {ttNumber: film.ttNumber, title: film.title, date: film.date, length: film.length, director: film.director, description: film.description, averageRating: average};
+//             console.log("filmWithAverage: " + filmWithAverage);
+//             callback();
+//             return filmWithAverage;
+//         }
+//     });
+// }
+
+/**
+ * Get request that gets all the users ratings
+ */
+router.get("/myRatings", function (req, res) {
+    //check authorization
+    var token = req.header("authorization");
+    jwt.verify(token, req.app.get('private-key'), function (err, decoded) {
+        if (err) {
+            res.status(401).json({error: "invalide authentication"})
+        } else {
+            User.find({username: decoded.username}, function (userErr, user) {
+                if (userErr) {
+                    res.status(400).json({error: "No such user found"})
+                } else {
+                    Rating.find({userId: user._id}, function (err, result) {
+                        if (err) {
+                            res.status(400).json({error: "error finding ratings"})
+                        } else {
+                            res.status(200).json(result);
+                        }
+                    })
+                }
+            })
+        }
+    })
+});
+
+/**
+ * post request to change a rating, get the new rating and film from the body
+ */
+router.put("/change", function (req, res) {
+    //check authorization
+    var token = req.header("authorization");
+    jwt.verify(token, req.app.get('private-key'), function (err, decoded) {
+        if (err) {
+            res.status(401).json({error: "invalide authentication"});
+        } else {
+            //find the user
+            User.findOne({username: decoded.username}, function (err, user) {
+                if (err) {
+                    res.status(400).json({'error': err.message});
+                } else {
+                    Rating.update({
+                        userId: user._id,
+                        ttNumber: req.body.ttNumber
+                    }, {'$set': {rating: req.body.rating}}, function (err) {
+                        if (err) {
+                            res.status(400).json({'error': err.message});
+                        } else {
+                            res.status(200).send("Data changed");
+                        }
+                    });
+                }
             });
         }
     });
 });
 
-function getGemiddeldeRating(film, foundTtNumber, i) {
-    Rating.find({ttNumber: foundTtNumber}, function (err, ratings) {
+/**
+ * Delete request to delete a rating, gets the movie ttNumber from the body
+ */
+router.delete('/delete', function (req, res) {
+    //check authorization
+    var token = req.header("authorization");
+    jwt.verify(token, req.app.get('private-key'), function (err, decoded) {
         if (err) {
-            res.status(400).json({error: "Could not find ratings"})
+            res.status(401).json({error: "invalide authentication"});
         } else {
-            //calculate the average
-            var average = 0;
-            for (j = 0; j < ratings.length; j++) {
-                average = average + ratings[j].rating;
-                console.log("!!!!!" + j);
-            }
-
-            average = average / j;
-            console.log("j = " + j);
-
-            var filmWithAverage = {film: film, averageRating: average};
-            console.log("filmWithAverage: " + filmWithAverage);
-            return filmWithAverage;
-        }
-    });
-}
-
-        /**
-         * Get request that gets all the users ratings
-         */
-        router.get("/myRatings", function (req, res) {
-            //check authorization
-            var token = req.header("authorization");
-            jwt.verify(token, req.app.get('private-key'), function (err, decoded) {
+            User.find({username: decoded.username}, function (err, user) {
                 if (err) {
-                    res.status(401).json({error: "invalide authentication"})
+                    res.status(400).json({error: "Could not find user in the database"});
                 } else {
-                    User.find({username: decoded.username}, function (userErr, user) {
-                        if (userErr) {
-                            res.status(400).json({error: "No such user found"})
-                        } else {
-                            Rating.find({userId: user._id}, function (err, result) {
-                                if (err) {
-                                    res.status(400).json({error: "error finding ratings"})
-                                } else {
-                                    res.status(200).json(result);
-                                }
-                            })
-                        }
+                    Rating.find({userId: user._id, ttNumber: req.body.ttNumber}, function (err, rating) {
+                        Rating.remove({_id: rating._id}, function (err) {
+                            if (err) {
+                                res.status(422).json({error: "Could not delete rating"})
+                            } else {
+                                res.status(200).json({result: "Rating removed"})
+                            }
+                        })
                     })
                 }
-            })
-        });
-
-        /**
-         * post request to change a rating, get the new rating and film from the body
-         */
-        router.put("/change", function (req, res) {
-            //check authorization
-            var token = req.header("authorization");
-            jwt.verify(token, req.app.get('private-key'), function (err, decoded) {
-                if (err) {
-                    res.status(401).json({error: "invalide authentication"});
-                } else {
-                    //find the user
-                    User.findOne({username: decoded.username}, function (err, user) {
-                        if (err) {
-                            res.status(400).json({'error': err.message});
-                        } else {
-                            Rating.update({
-                                userId: user._id,
-                                ttNumber: req.body.ttNumber
-                            }, {'$set': {rating: req.body.rating}}, function (err) {
-                                if (err) {
-                                    res.status(400).json({'error': err.message});
-                                } else {
-                                    res.status(200).send("Data changed");
-                                }
-                            });
-                        }
-                    });
-                }
             });
-        });
+        }
+    });
+});
 
-        /**
-         * Delete request to delete a rating, gets the movie ttNumber from the body
-         */
-        router.delete('/delete', function (req, res) {
-            //check authorization
-            var token = req.header("authorization");
-            jwt.verify(token, req.app.get('private-key'), function (err, decoded) {
-                if (err) {
-                    res.status(401).json({error: "invalide authentication"});
-                } else {
-                    User.find({username: decoded.username}, function (err, user) {
-                        if (err) {
-                            res.status(400).json({error: "Could not find user in the database"});
-                        } else {
-                            Rating.find({userId: user._id, ttNumber: req.body.ttNumber}, function (err, rating) {
-                                Rating.remove({_id: rating._id}, function (err) {
-                                    if (err) {
-                                        res.status(422).json({error: "Could not delete rating"})
-                                    } else {
-                                        res.status(200).json({result: "Rating removed"})
-                                    }
-                                })
-                            })
-                        }
-                    });
-                }
-            });
-        });
+router.get('/all', function (req, res) {
+    console.log('films');
+    Rating.find({}).exec(function (err, films) {
+        if (err) {
+            res.status(500).json({'error': 'Could not load films from database'});
+            return
+        }
+        res.status(200).json(films)
+    });
+});
 
-        router.get('/all', function (req, res) {
-            console.log('films');
-            Rating.find({}).exec(function (err, films) {
-                if (err) {
-                    res.status(500).json({'error': 'Could not load films from database'});
-                    return
-                }
-                res.status(200).json(films)
-            });
-        });
-
-        module.exports = router;
+module.exports = router;
